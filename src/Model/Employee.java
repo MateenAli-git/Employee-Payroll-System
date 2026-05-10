42547f098cbab57acec36f6846a53df49f7786bd
MateenAli-git <mateenalipro43@email.com>
2026-05-10 16:00:15 +0500

    Made Employee and Employee View class

--- src/Model/Employee.java
+++ src/Model/Employee.java
@@ -1,5 +1,70 @@
 package Model;
 
 public class Employee {
-    
+
+    private int id;
+    private String name;
+    private String department;
+    private double basicSalary;
+    private double bonus;
+    private double deduction;
+
+    public Employee(int id,
+                    String name,
+                    String department,
+                    double basicSalary,
+                    double bonus,
+                    double deduction) {
+
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.basicSalary = basicSalary;
+        this.bonus = bonus;
+        this.deduction = deduction;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getBasicSalary() {
+        return basicSalary;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public double getDeduction() {
+        return deduction;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setBasicSalary(double basicSalary) {
+        this.basicSalary = basicSalary;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public void setDeduction(double deduction) {
+        this.deduction = deduction;
+    }
 }
--- src/View/EmployeeView.java
+++ src/View/EmployeeView.java
@@ -1,5 +1,22 @@
 package View;
 
+import Model.Employee;
+
 public class EmployeeView {
-    
+
+    public void displayEmployee(Employee employee,
+                                double netSalary,
+                                double tax) {
+
+        System.out.println("\n----------------------------");
+        System.out.println("Employee ID : " + employee.getId());
+        System.out.println("Name        : " + employee.getName());
+        System.out.println("Department  : " + employee.getDepartment());
+        System.out.println("Salary      : " + employee.getBasicSalary());
+        System.out.println("Bonus       : " + employee.getBonus());
+        System.out.println("Deduction   : " + employee.getDeduction());
+        System.out.println("Net Salary  : " + netSalary);
+        System.out.println("Tax         : " + tax);
+        System.out.println("----------------------------");
+    }
 }
